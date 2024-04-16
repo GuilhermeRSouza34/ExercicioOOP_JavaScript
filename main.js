1e08d08079be4408bc2664b07283ce6b90fb238e
Guilherme <guilhermersouza34@gmail.com>
2024-04-16 12:50:30 -0300

    Criando a Classe Animal

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-console.log("");
-console.log("");
+// CLASES ABSTRATAS
+class Animal {
+    constructor(nome, idade) {
+        if (this.constructor === Animal) {
+            throw new Error("Não foi possível instanciar classe abstrata")
+        }
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
