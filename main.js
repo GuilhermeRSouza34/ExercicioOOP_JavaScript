92d39fbeeac7d00f8644c9ae1deab06d2c908fc5
Guilherme <guilhermersouza34@gmail.com>
2024-04-16 13:07:30 -0300

    Criando a Classe Moto e As instancias

--- main.js
+++ main.js
@@ -20,4 +20,31 @@ class Carro extends Veiculo {
         console.log(`Ano: ${this.ano} Cor: ${this.cor}`);
     }
 }
+
+class Moto extends Veiculo {
+    constructor(marca, modelo, cilindradas) {
+        super(marca, modelo);
+        this.cilindradas = cilindradas;
+    }
+
+    exibirInformacoes() {
+        super.exibirInformacoes();
+        console.log(`Cilindradas: ${this.cilindradas}`);
+    }
+}
+
+// Criando instâncias de objetos
+let carro1 = new Carro("Toyota", "Corolla", "Preto");
+let moto1 = new Moto("Honda", "CBR 600RR", "600");
+let carro2 = new Carro("Ford", "Fusion", "Prata");
+
+// Exibindo informações dos objetos
+console.log("Informações do Carro 1:");
+carro1.exibirInformacoes();
+
+console.log("\nInformações da Moto 1:");
+moto1.exibirInformacoes();
+
+console.log("\nInformações do Carro 2:");
+carro2.exibirInformacoes();
     
