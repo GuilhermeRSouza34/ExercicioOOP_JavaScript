d566792d62b403eacfb1a5945eac1756254991c1
Guilherme <guilhermersouza34@gmail.com>
2024-04-16 13:02:22 -0300

    Criando a Classe Carro

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ class Veiculo {
         this.marca = marca;
         this.modelo = modelo;
     }
+    exibirInformacoes() {
+        console.log(`Marca: ${this.marca} Modelo: ${this.modelo}`);
+    }
 }
+
+class Carro extends Veiculo {
+    constructor(marca, modelo, ano, cor) {
+        super(marca, modelo);
+        this.ano = ano;
+        this.cor = cor;
+    }
     
